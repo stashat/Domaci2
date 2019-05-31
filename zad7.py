7cbd4449bbdab75575e4a9bc45a3e37b6f80b023
Stasha Tadic <stasa.tadic@gmail.com>
2019-05-31 11:38:53 +0200

    ''

--- zad7.py
+++ zad7.py
@@ -1,12 +1,25 @@
-#
-i = input("Unesite br igrice: ")
+#i = int(input("Unesite br igrice: "))
 one_game = []
+imena = []
+ocjene = []
+godine = []
 with open("igrice.txt", 'r') as f:
-    #for line in f:
-        igrice = f.readlines()
-        igrice = [x.strip() for x in igrice]
-        print(igrice)
+    igrice = f.readlines()
+    igrice = [x.strip() for x in igrice]
+    print(igrice)
+    for i in igrice:
+        one_game = i.split(';')
+        imena.append(one_game[0])
+        ocjene.append(one_game[1])
+        godine.append(one_game[2])
+        
+        print(one_game)
 
+print(imena)
+print(ocjene)
+print(godine)
 
-one_game.append(igrice[i])
-print(one_game)
+imena_filter = list(filter(lambda x: 2<len(x)<50, imena ))
+print(imena_filter)
+ocjene_filter = list(filter(lambda x: 1<x<10, ocjene))
+print(ocjene_filter)
