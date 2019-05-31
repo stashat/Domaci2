435b9a3bf6d8f74f6f38969dbd408e4121384d38
Stasha Tadic <stasa.tadic@gmail.com>
2019-05-31 11:14:26 +0200

    ''

--- zad7.py
+++ zad7.py
@@ -1,6 +1,12 @@
 #
-# unos = input("Unesite igricu: ")
-
+i = input("Unesite br igrice: ")
+one_game = []
 with open("igrice.txt", 'r') as f:
-    for line in f:
-        print(line, end = '')
+    #for line in f:
+        igrice = f.readlines()
+        igrice = [x.strip() for x in igrice]
+        print(igrice)
+
+
+one_game.append(igrice[i])
+print(one_game)
